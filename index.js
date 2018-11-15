09b67649669fe91fee1b1a1d1c3ae4a0a8d9601b
Gennadiy <g.abashkin@petrovich.ru>
2018-11-15 19:24:06 +0300

    Added new arr and check directory or file callback

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 const source = './source-folder';
+const newFolder = './new-folder';
 
-const files = fs.readdir(source, (error, files) => {
+
+fs.readdir(source, (error, files) => {
+
     if (error) {
         console.log('Error');
         return;
     }
 
-    fs.mkdir(path.join(__dirname, 'new-folder'), (error) => {
-        if (error) {
-            console.log('Folder can not be created');
-            return;
-        }
-    });
+    // if (files) {
+    //   fs.mkdir(path.join(__dirname, 'new-folder'), (error) => {
+    //     if (error) {
+    //       console.log('Folder can not be created');
+    //       return;
+    //     }
+    //   });
+    // }
+
+    const arr = [];
+
+    function isFileCheck (files) {
+      files.forEach((file) => {
+        fs.stat(path.join(__dirname, source, file), (error, stat) => {
+          if (stat.isFile()) {
+            arr.push(stat);
+          } else if (stat.isDirectory){
+          //    code
+          }
+        })
+      })
+    }
+
+    isFileCheck(files);
 
 
 });
